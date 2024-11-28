15fcaefb48bd303ac0ec7439f2debb3984163706
Gregory Sizer <sizerg704@students.cochise.edu>
2024-11-28 02:37:38 -0700

    package for use
    Releases for windows linux and godot pck

--- main.gd
+++ main.gd
@@ -5,23 +5,29 @@ extends Control
 @export var ResultList: ItemList
 var _vscroll : VScrollBar
 
-enum Dice { D4=4, D6=6, D8=8, D10=10, D20=20, D100=100 }
+enum Dice { D4=4, D6=6, D8=8, D10=10, D12=12, D20=20, D100=100 }
 
 var ResultString : String = "{i}:{r}"
 var RollString : String = "Rolled {C}D{F} = {S} | Results: {R}"
-var RollHistory : Array = []
 var LastRoll : Dictionary = { "Count"=0, "Die"=Dice.D4, "Results"=_lastResults, "Total"=0 }
 
 var _count : int = 1
 var _face := Dice.D4
-var _lastResults : Array = []
+var _lastResults : Array[int] = []
 var _total : int = 0
 
 func _ready():
-	RollHistory.clear()
 	_lastResults.clear()
 	ResultList.clear()
 	_vscroll = ResultList.get_v_scroll_bar()
+	DieList.add_item("D4")
+	DieList.add_item("D6")
+	DieList.add_item("D8")
+	DieList.add_item("D10")
+	DieList.add_item("D12")
+	DieList.add_item("D20")
+	DieList.add_item("D100")
+	ResultList.add_item("Click a result to copy it.")
 
 func Roll( Count:int, Die:int) -> Array:
 	_lastResults.clear()
@@ -54,8 +60,9 @@ func _on_die_option_item_selected(index):
 		1: _face=Dice.D6
 		2: _face=Dice.D8
 		3: _face=Dice.D10
-		4: _face=Dice.D20
-		5: _face=Dice.D100
+		4: _face=Dice.D12
+		5: _face=Dice.D20
+		6: _face=Dice.D100
 	print_debug(_face)
 
 func _on_result_list_item_selected(index):
